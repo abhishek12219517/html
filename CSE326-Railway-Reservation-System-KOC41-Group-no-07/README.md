5d46a6ae10952b1fdaf7840695ce34b3037b9902
abhishek12219517 <119564370+abhishek12219517@users.noreply.github.com>
2022-12-22 11:07:42 +0530

    Add files via upload
    This is a railway booking website by using Html , CSS, javaScript

---
+++ CSE326-Railway-Reservation-System-KOC41-Group-no-07/README.md
@@ -0,0 +1,2 @@
+
+CSE326 / KOC41 / Group no. 07/ Reg numbers -12221808, 12219364, 12219517  /  Roll numbers - 20,21,22
---
+++ CSE326-Railway-Reservation-System-KOC41-Group-no-07/onclick.js
@@ -0,0 +1,22 @@
+function newtab1() {
+    window.open("testing.html", "_self")
+};
+
+function newtab2() {
+    window.open("Meals.html", "_self")
+};
+
+function newtab3() {
+    window.open("holiday packages.html", "_self")
+
+};
+
+function newtab4() {
+    window.open("service.html", "_self")
+
+};
+
+function newtab5() {
+
+    window.open("contact.html", "_self")
+};
